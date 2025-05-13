e96380f0d1efe9176c8f37fa7227d79783256efc
ly61 <2058671239@qq.com>
2025-05-13 09:00:24 +0800

    rune_final

---
+++ rm_vision/rm_power_rune/include/PowerRune.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Calculator.h"
+#include "Detector.h"
+#include "Param.h"
+#include "Utility.h"
+
+#define CONFIG_PATH "/config/config.yaml"
+
+namespace power_rune {
+
+
+
+class PowerRune {
+   public:
+    PowerRune();
+    bool runOnce(const cv::Mat& image, double pitch, double yaw, double roll = 0.0);
+
+    cv::Mat get_img_show(){return m_detector.get_imgShow();}
+    cv::Mat get_img_arrow(){return m_detector.get_imgArrow();}
+    cv::Mat get_img_armor(){return m_detector.get_imgArmor();}
+    cv::Mat get_img_src(){return m_detector.get_imgsrc();}
+
+
+   public:
+    Param m_param;
+    Detector m_detector;
+    Calculator m_calculator;
+};
+
+}  // namespace power_rune
